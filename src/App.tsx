133e176530e64f7effeec514c0c3857ddf6c8d1e
muhidayat03 <muhidayat03@gmail.com>
2026-06-01 22:00:26 +0700

    setup routes and stub pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-function App() {
-  return <></>
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
+import { HomePage, BuilderPage, PlayerPage, ResultsPage, NotFoundPage } from './pages'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/play" replace />} />
+        <Route path="/play" element={<HomePage />} />
+        <Route path="/build" element={<HomePage />} />
+        <Route path="/builder" element={<BuilderPage />} />
+        <Route path="/builder/:id" element={<BuilderPage />} />
+        <Route path="/quiz/:id" element={<PlayerPage />} />
+        <Route path="/quiz/:id/results" element={<ResultsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as HomePage } from './HomePage'
+export { default as BuilderPage } from './BuilderPage'
+export { default as PlayerPage } from './PlayerPage'
+export { default as ResultsPage } from './ResultsPage'
+export { default as NotFoundPage } from './NotFoundPage'
